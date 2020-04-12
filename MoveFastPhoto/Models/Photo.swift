2107d9c3f145a83e5119c7a25bbad1b63c074ab9
Diego Manansala <diego.manansala@seer-technologies.com>
2020-04-12 18:21:45 +0900

    - Add initial Photo model

---
+++ MoveFastPhoto/Models/Photo.swift
@@ -0,0 +1,11 @@
+//
+//  Photo.swift
+//  MoveFastPhoto
+//
+//  Created by Diego Karlo Manansala on 12/04/2020.
+//  Copyright © 2020 Diego Karlo Manansala. All rights reserved.
+//
+
+struct Photo {
+    let photoUrl: String
+}
