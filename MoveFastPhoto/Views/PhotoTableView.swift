ceb26b173b42433fa984293054ff6c94ee1bc4da
Diego Manansala <diego.manansala@seer-technologies.com>
2020-04-14 13:52:45 +0900

    - Transfer UITableView extension functions from view controller to appropriate custom table view

--- MoveFastPhoto/Views/PhotoTableView.swift
+++ MoveFastPhoto/Views/PhotoTableView.swift
@@ -8,6 +8,44 @@
 
 import UIKit
 
+// based on https://stackoverflow.com/a/45157417
+extension UITableView {
+
+    
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: "HelveticaNeue", size: 15)
+        messageLabel.sizeToFit()
+
+        DispatchQueue.main.async {
+            self.backgroundView = messageLabel
+        }
+    }
+    
+    func displayLoadingIndicator() {
+        let loadingIndicatorView = UIView(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        loadingIndicatorView.sizeToFit()
+        let spinner = UIActivityIndicatorView.init(style: UIActivityIndicatorView.Style.large)
+        spinner.startAnimating()
+        spinner.center = loadingIndicatorView.center
+        loadingIndicatorView.addSubview(spinner)
+        
+        DispatchQueue.main.async {
+            self.backgroundView = loadingIndicatorView
+        }
+    }
+
+    func restore() {
+        DispatchQueue.main.async {
+            self.backgroundView = nil
+        }
+    }
+}
+
 class PhotoTableView: UITableView {
 
     /*
@@ -17,5 +55,13 @@ class PhotoTableView: UITableView {
         // Drawing code
     }
     */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.separatorStyle = .none
+    }
+    
+    
 
 }
