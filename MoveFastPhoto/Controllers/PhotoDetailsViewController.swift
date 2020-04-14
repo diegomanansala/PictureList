5e88126048010b03c02a21ccd8633c1975d77645
Diego Manansala <diego.manansala@seer-technologies.com>
2020-04-15 02:01:53 +0900

    - Add Photo details view

---
+++ MoveFastPhoto/Controllers/PhotoDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoDetailsViewController.swift
+//  MoveFastPhoto
+//
+//  Created by Diego Karlo Manansala on 15/04/2020.
+//  Copyright © 2020 Diego Karlo Manansala. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MoveFastPhoto/Views/PhotoDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoDetailsView.swift
+//  MoveFastPhoto
+//
+//  Created by Diego Karlo Manansala on 15/04/2020.
+//  Copyright © 2020 Diego Karlo Manansala. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
